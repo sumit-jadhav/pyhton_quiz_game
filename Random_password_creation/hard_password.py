a9b4b2d8e1fb16c3c2416e21c1647802ef33d1a1
sumit-jadhav <sumitja232@gmail.com>
2022-12-02 20:53:13 +0530

    day_5

--- Random_password_creation/hard_password.py
+++ Random_password_creation/hard_password.py
@@ -14,7 +14,6 @@ nr_letters = int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
-#Eazy Level - Order not randomised:
 password_list = []
 
 for char in range(1, nr_letters + 1):
